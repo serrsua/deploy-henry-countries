13f4d74a0512ad4ade1bd0bc627b3d2699c47e10
Sergio Suárez <serrsua.88@gmail.com>
2023-03-17 13:22:49 -0300

    detail fix

--- client/src/views/Detail/Detail.jsx
+++ client/src/views/Detail/Detail.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useParams } from "react-router-dom";
 import { getCountry } from "../../redux/actions";
@@ -7,33 +7,36 @@ import styles from "./Detail.module.css";
 const Detail = () => {
   const dispatch = useDispatch();
   const { id } = useParams();
+  const [detail, setDetail] = useState()
+  const country = useSelector((state) => state.detailCountry);
 
   useEffect(() => {
     dispatch(getCountry(id));
+    setDetail(country)
+    return () => {setDetail({})}
   }, [dispatch, id]);
 
-  const country = useSelector((state) => state.detailCountry);
 
   return (
     <>
       <div className={styles.container}>
         <div className={styles.detailContainer}>
           <div className={styles.information}>
-            <img className={styles.flagImg} src={country?.image} alt="flag" />
-            <h3>ID: {country?.id}</h3>
-            <h3>Name: {country?.name}</h3>
-            <h3>Continent: {country?.continents}</h3>
-            <h3>Capital: {country?.capital}</h3>
-            {country.subregion && <h3>Subregion: {country.subregion}</h3>}
-            {country.area && <h3>Area: {country.area} Km²</h3>}
-            <h3>Population: {country?.population}</h3>
+            <img className={styles.flagImg} src={detail?.image} alt="flag" />
+            <h3>ID: {detail?.id}</h3>
+            <h3>Name: {detail?.name}</h3>
+            <h3>Continent: {detail?.continents}</h3>
+            <h3>Capital: {detail?.capital}</h3>
+            {detail.subregion && <h3>Subregion: {detail.subregion}</h3>}
+            {detail.area && <h3>Area: {detail.area} Km²</h3>}
+            <h3>Population: {detail?.population}</h3>
           </div>
 
           <div className={styles.actContainer}>
             <h2>Activities</h2>
             <div className={styles.cardsContainer}>
-              {country.activities?.length ? (
-                country.activities.map((activity) => {
+              {detail.activities?.length ? (
+                detail.activities.map((activity) => {
                   return (
                     <div className={styles.activities} key={activity.id}>
                       <h3>{activity.name.toUpperCase()}</h3>
