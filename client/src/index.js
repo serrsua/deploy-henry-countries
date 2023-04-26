d60399244e7716432a50e7113e50921f6be48ef3
Sergio Suárez <serrsua.88@gmail.com>
2023-04-26 12:34:25 -0300

    deploy render

--- client/src/index.js
+++ client/src/index.js
@@ -8,8 +8,8 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import axios from "axios";
 
-/* axios.defaults.baseURL = 'http://localhost:3001'; */
-axios.defaults.baseURL = 'https://deploy-henry-countries-production.up.railway.app/';
+axios.defaults.baseURL = 'http://localhost:3001';
+// axios.defaults.baseURL = 'https://deploy-henry-countries-production.up.railway.app/';
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
