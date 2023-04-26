681689c06bfc07c40a90cd8828e1e162736af0b8
Sergio Suárez <serrsua.88@gmail.com>
2023-04-26 13:31:02 -0300

    Back hacia Render

--- client/src/index.js
+++ client/src/index.js
@@ -8,8 +8,9 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:3001';
+// axios.defaults.baseURL = 'http://localhost:3001';
 // axios.defaults.baseURL = 'https://deploy-henry-countries-production.up.railway.app/';
+axios.defaults.baseURL = "https://countries-back-pxz3.onrender.com"
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
